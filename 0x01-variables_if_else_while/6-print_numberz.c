dd888ab896f00141aa6edd578828b480ca772e74
basitali2779 <basitali2779@gmail.com>
2022-04-08 19:49:41 -0700

    task 6 updated

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,16 +5,16 @@
 *Return: Always 0 (Success)
 */
 
-int main (void)
+int main ( void )
 {
 	int n = '0';
 
-	while (n <= '9')
+	while ( n <= '9' )
 	{
-		putchar(n);
+		putchar ( n );
 		n++;
 	}
-	putchar ('\n');
+	putchar ( '\n' );
 
-	return (0);
+	return ( 0 );
 }
